39674bb9d5d66a5b688e24d43906384d257bb37d
taches-ai <thomasrobert@duck.com>
2024-10-11 23:28:36 +0200

    Fixed imports

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
-from src.scene import Scene
-from src.composition.composition import Composition
-from src.action.action import Action
-from src.subject.character import Character
-from src.environment.environment import Environment
+from .src.scene import Scene
+from .src.composition.composition import Composition
+from .src.action.action import Action
+from .src.subject.character import Character
+from .src.environment.environment import Environment
 
 NODE_CLASS_MAPPINGS = {
     "Scene": Scene,
     "Composition": Composition,
     "Action": Action,
-    "subject": Character,
+    "Character": Character,
     "Environment": Environment,
 }
 

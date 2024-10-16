220efd9698adc15f5486d07e0bdb2a1e76c80c7c
taches-ai <thomasrobert@duck.com>
2024-10-16 20:40:38 +0200

    Add input to action node

--- src/action/action.py
+++ src/action/action.py
@@ -10,6 +10,32 @@ class Action(Node):
 
     CATEGORY = Node.CATEGORY + "/Components"
 
+    @classmethod
+    def INPUT_TYPES(cls):
+        inputs = super().INPUT_TYPES()
+
+        action_data = cls().components["action"].data
+        action_list = action_data.keys()
+        action_list.insert(0, "random")
+
+        # Update the required inputs
+        required_inputs = {
+            "action_type": (action_list,),
+            "seed": inputs["required"]["seed"]
+        }
+        inputs["required"] = required_inputs
+        return inputs
+
+    def run_node(self, action_type):
+
+        if action_type == "random":
+            action_type = self.select_tags(self.data)
+
+        data = self.data[action_type]
+        prompt = self.select_tags(data)
+
+        return (prompt,)
+
     def build_prompt(self):
         data = get_nested_dict_value(self.data, self.type)
         action = self.select_tags(data)

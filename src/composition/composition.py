9df2cf34b857dd0271fc6fb8770b33129f9ee1cd
taches-ai <thomasrobert@duck.com>
2024-10-16 22:40:27 +0200

    Add inputs for protagonists and suffix

--- src/composition/composition.py
+++ src/composition/composition.py
@@ -3,10 +3,40 @@ from ..node import Node
 
 class Composition(Node):
 
-    def __init__(self, seed):
+    def __init__(self, seed=0):
         super().__init__(seed, data_file="composition.toml")
 
     CATEGORY = Node.CATEGORY + "/Components"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        inputs = super().INPUT_TYPES()
+
+        seed = inputs["required"]["seed"]
+        composition_data = cls(seed).data
+        prefix = composition_data["prefix"]
+        protagonists = list(composition_data["protagonists"])
+        protagonists.insert(0, "random")
+
+        # Update the required inputs
+        required_inputs = {
+            "prefix": ("STRING", {"default": prefix, "multiline": True}),
+            "protagonists": (protagonists,),
+            "seed": seed
+        }
+        inputs["required"] = required_inputs
+        return inputs
+
+    def run_node(self, prefix, protagonists, seed):
+
+        self.data["prefix"] = prefix
+
+        if protagonists != "random":
+            self.data["protagonists"] = protagonists
+
+        self.update_seed(seed)
+        prompt = self.get_prompt()
+        return (prompt,)
 
     def build_prompt(self):
         super().build_prompt()

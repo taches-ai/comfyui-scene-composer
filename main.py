90bf41bd290ec503190b7dabec837e9da8ced772
taches-ai <thomasrobert@duck.com>
2024-10-16 22:17:23 +0200

    Simplify main (testing purpose only)

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 from src.scene import Scene
 
-from src.action.action import Action
-
 
 def main():
 
     # Simluate a Scene node with seed
-    scene = Scene(seed=1)
-    print(f"Seed: {scene.seed}\n{scene}")
-
-    # Change the seed of the whole scene
-    scene.update_seed(2)
-    print(f"Seed: {scene.seed}\n{scene}")
-
-    # Change Action seed to match the first one
-    scene.components["action"] = Action(seed=1)
-    print(f"Action changed: {scene.components['action']}")
-
-    # Define the action type
-    scene.components["action"].type = "drinking"
-    print(f"Action changed: {scene.components['action']}")
+    scene = Scene()
+    print(scene)
 
 
 if __name__ == "__main__":

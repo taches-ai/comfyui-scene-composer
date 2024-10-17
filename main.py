482a880fe9b6feab143208a3603137f44d4ad982
taches-ai <thomasrobert@duck.com>
2024-10-17 21:28:55 +0200

    Add CLI parameters for node simulation

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
+import argparse
+import random
 from src.scene import Scene
 
 
-def main():
+def main(node, seed=0):
 
-    # Simluate a Scene node with seed
-    scene = Scene()
-    print(scene)
+    if not seed:
+        seed = random.randint(0, 0xffffffffffffffff)
+
+    scene = Scene(seed)
+
+    if node:
+        output = scene.components[node]
+
+    output = scene
+
+    print(f"SEED: {seed}")
+    print(f"TAGS: {output}")
 
 
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser(description="Simulate a scene")
+
+    parser.add_argument(
+        "--seed", "-s",
+        type=int,
+        default=0,
+        help="Seed for the node"
+    )
+
+    parser.add_argument(
+        "--node", "-n",
+        type=str,
+        choices=["composition", "subject", "action", "environment"],
+        help="Name of the node to simulate"
+    )
+
+    args = parser.parse_args()
+    main(args.node, args.seed)

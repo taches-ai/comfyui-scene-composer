7160f4999503be85113dacf484c968a57481aa52
taches-ai <thomasrobert@duck.com>
2024-10-22 21:06:18 +0200

    Integrate breasts to body class

--- src/components/character/body.py
+++ src/components/character/body.py
@@ -11,26 +11,13 @@ class Body(Node):
         type = self.select_tags(data["types"])
         colors = self.select_tags(data["colors"])
         color = f"{colors} skin" if colors else ""
-        breasts = Breasts(self.seed)
-        breasts = breasts.build_prompt()[0]
+
+        breasts_data = self.data["body"]["breasts"]
+        breasts_size = self.select_tags(breasts_data["sizes"])
+        breasts_prompt = f"{breasts_size}"
+
         extras = self.select_tags(data["extras"])
 
-        components = [type, color, breasts, extras]
+        components = [type, color, breasts_prompt, extras]
         prompt = self.stringify_tags(components)
-        return (prompt,)
-
-
-class Breasts(Node):
-
-    def __init__(self, seed):
-        super().__init__(seed, data_file="character.toml")
-
-    def build_prompt(self):
-        data = self.data["body"]["breasts"]
-        size = self.select_tags(data["sizes"])
-
-        # TODO: Handle nipple display according to clothes state
-        # nipples = self.select_tags(self.data["nipples"])
-
-        prompt = f"{size}"
         return (prompt,)

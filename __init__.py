b82192408ae7f82474805e426ab8a84b793520e6
mus-taches <thomasrobert@duck.com>
2024-11-10 16:19:01 +0100

    fix: node mapping

--- __init__.py
+++ __init__.py
@@ -4,16 +4,35 @@ from .py.components.action import Action
 from .py.components.character.character import Character
 from .py.components.clothes import Clothes
 from .py.components.environment import Environment
+import os
 
+# Load CozySpoke
+# Will change widgets by using API calls
+cozy_spoke_path = os.path.join(
+    os.path.dirname(__file__), 'py', 'cozy_spoke.py')
+with open(cozy_spoke_path) as f:
+    exec(f.read())
+
+# Mappings
 NODE_CLASS_MAPPINGS = {
-    "🎞️ Scene": Scene,
-    "📹 Composition": Composition,
-    "🎬 Action": Action,
-    "👤 Character": Character,
-    "👕 Clothes": Clothes,
-    "⛅️ Environment": Environment,
+    "Scene": Scene,
+    "Composition": Composition,
+    "Action": Action,
+    "Character": Character,
+    "Clothes": Clothes,
+    "Environment": Environment,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Scene": "🎞️ Scene",
+    "Composition": "📹 Composition",
+    "Action": "🎬 Action",
+    "Character": "👤 Character",
+    "Clothes": "👕 Clothes",
+    "Environment": "⛅️ Environment",
 }
 
 WEB_DIRECTORY = "js"
 
-__all__ = ['NODE_CLASS_MAPPINGS', "WEB_DIRECTORY"]
+__all__ = ['NODE_CLASS_MAPPINGS',
+           "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
